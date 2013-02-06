bae215a4a498b112783ebe1afd67b7e6829de003
Nikhil <jkdman63@gmail.com>
2013-02-05 19:41:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webhero::Application.config.secret_token = '9a4058010180e48da27c6e076a4df6dd96b02a01692a1a4baac1771039203846aa61a912d2e2e0c0ea1839ec0f22f925eb3f045cde55eebd4317b9421167294c'
